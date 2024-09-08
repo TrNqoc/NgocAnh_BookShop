c5ed782d98460c6fc5f8ebafe3efab03d4098261
TrNqoc <truongngocdth@gmail.com>
2024-09-08 15:05:39 +0700

    change title

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>Trang Chủ | Cửa hàng sách</title>
+	<title>Ngọc Anh BookShop</title>
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
